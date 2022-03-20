be3324e8c262d9231c83b08cea38d1bd517fc518
TariQMahEmbrace <tad@embrace-it.com>
2022-03-20 15:58:04 +0500

    Blog App with Sanity

---
+++ typings.d.ts
@@ -0,0 +1,35 @@
+export interface Post {
+  _id: string
+  title: string
+  _createdAt: string
+  author: {
+    name: string
+    image: string
+  }
+  description: string
+  mainImage: {
+    asset: {
+      url: string
+    }
+  }
+  slug: {
+    current: string
+  }
+  body: [object]
+  comment: Comment[]
+}
+
+export interface Comment {
+  approved: boolean
+  comment: string
+  email: string
+  post: {
+    _ref: string
+    _type: string
+  }
+  _createdAt: string
+  _id: string
+  _rev: string
+  _type: string
+  _updatedAt: string
+}
